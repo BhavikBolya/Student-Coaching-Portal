11c78a358802ebc5764b4749187a6f73cfd54772
Bhavik Bolya <bhavikbolya2911@gmail.com>
2021-08-30 13:51:22 +0530

    Update register.php

--- register.php
+++ register.php
@@ -237,7 +237,17 @@ if(isset($_POST['submit2'])){
   $gen= $_POST["gender"];
   $em= $_POST["email"];
   $fee=$_POST["paisa"];
-  $conn = mysqli_connect("localhost", "root", "", "valorant");
+  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $cleardb_server = $cleardb_url["host"];
+  $cleardb_username = $cleardb_url["user"];
+  $cleardb_password = $cleardb_url["pass"];
+  $cleardb_db = substr($cleardb_url["path"],1);
+  $active_group = 'default';
+  $query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+  // $conn = mysqli_connect("localhost", "root", "", "valorant");
 
   if (!$conn) {
     die("Error connecting to database: " . mysqli_connect_error());
